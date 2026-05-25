3a864ee4fd9738e47ded75766acf28adeaecb0f3
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-05-25 13:50:33 +0800

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 import './App.css'
 
 function App() {
   return (
     <div className="app">
+        <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Auth />} />
